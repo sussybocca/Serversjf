8d37e512e12cf3c5220d9b08aa2ba24abdb87243
sussybocca <babyyodacutefry@gmail.com>
2025-10-29 11:40:44 -0500

    Update PostList.jsx

--- PostList.jsx
+++ PostList.jsx
@@ -1,1 +1,18 @@
+import AddComment from "./AddComment.jsx";
+import CommentList from "./CommentList.jsx";
+
+export default function PostList({ posts, user, refreshPosts }) {
+  return (
+    <div>
+      {posts.map(post => (
+        <div key={post.id} style={{ border: "1px solid #ccc", padding: 10, margin: 10 }}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          <CommentList postId={post.id} />
+          <AddComment user={user} postId={post.id} onCommentAdded={refreshPosts} />
+        </div>
+      ))}
+    </div>
+  );
+}
 
