843068c9270fbaf04f41677131c86240bca16d87
sussybocca <babyyodacutefry@gmail.com>
2025-10-29 11:39:06 -0500

    Update getSecrets.js

--- secrets/getSecrets.js
+++ secrets/getSecrets.js
@@ -1,1 +1,9 @@
+export function getSecrets() {
+  return {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS
+  };
+}
 
