8e3e3b5d02885eed289bfe5be891f78dab36dbba
sussybocca <babyyodacutefry@gmail.com>
2025-10-29 11:38:52 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+import { getSecrets } from "./secrets/getSecrets.js";
+
+const { SUPABASE_URL, SUPABASE_KEY } = getSecrets();
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
