2851dda8497c2b96d5cf4df9930799415f3ad52d
sussybocca <babyyodacutefry@gmail.com>
2025-10-29 11:41:07 -0500

    Update CommentList.jsx

--- CommentList.jsx
+++ CommentList.jsx
@@ -1,1 +1,22 @@
+import { useEffect, useState } from "react";
+import { supabase } from "./supabaseClient.js";
+
+export default function CommentList({ postId }) {
+  const [comments, setComments] = useState([]);
+
+  const fetchComments = async () => {
+    const { data } = await supabase.from("comments").select("*").eq("post_id", postId);
+    setComments(data);
+  };
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  return (
+    <div style={{ marginLeft: 20 }}>
+      {comments.map(c => <p key={c.id}><strong>{c.user_id}:</strong> {c.content}</p>)}
+    </div>
+  );
+}
 
